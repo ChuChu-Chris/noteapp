a7bed11c7de9dd635b465c65c0b0ae26f53e15c7
ChuChu Chris <youngkryst@gmail.com>
2022-04-20 13:39:54 +0530

    step-2

--- lib/views/login_view.dart
+++ lib/views/login_view.dart
@@ -20,6 +20,7 @@ class _LoginViewState extends State<LoginView> {
   void initState() {
     _email = TextEditingController();
     _password = TextEditingController();
+    // ignore: todo
     // TODO: implement initState
     super.initState();
   }
@@ -28,6 +29,7 @@ class _LoginViewState extends State<LoginView> {
   void dispose() {
     _email.dispose();
     _password.dispose();
+    // ignore: todo
     // TODO: implement dispose
     super.dispose();
   }
@@ -36,69 +38,59 @@ class _LoginViewState extends State<LoginView> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text(
-            'Login'), //to get a default blue nav-bar with a title of register
+        title: const Text('Login'),
       ),
-      body: FutureBuilder(
-        future: Firebase.initializeApp(
-          options: DefaultFirebaseOptions.currentPlatform,
+      body: Column(children: [
+        Padding(
+          padding: const EdgeInsets.all(9.8),
+          child: TextField(
+            controller: _email,
+            keyboardType: TextInputType.emailAddress,
+            autocorrect: false,
+            enableSuggestions: false,
+            decoration: const InputDecoration(
+              hintText: "Enter Email Address",
+            ),
+          ),
         ),
-        builder: (BuildContext context, AsyncSnapshot<dynamic> snapshot) {
-          switch (snapshot.connectionState) {
-            case ConnectionState.done:
-              return Column(children: [
-                Padding(
-                  padding: const EdgeInsets.all(9.8),
-                  child: TextField(
-                    controller: _email,
-                    keyboardType: TextInputType.emailAddress,
-                    autocorrect: false,
-                    enableSuggestions: false,
-                    decoration: const InputDecoration(
-                      hintText: "Enter Email Address",
-                    ),
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(9.8),
-                  child: TextField(
-                    controller: _password,
-                    obscureText: true,
-                    decoration:
-                        const InputDecoration(hintText: "Enter Password"),
-                    enableSuggestions: false,
-                    autocorrect: false,
-                  ),
-                ),
-                TextButton(
-                  onPressed: () async {
-                    final email = _email.text;
-                    final password = _password.text;
-                    try {
-                      final UserCredential = await FirebaseAuth.instance
-                          .signInWithEmailAndPassword(
-                              email: email, password: password);
-                      print(UserCredential);
-                    } on FirebaseAuthException catch (e) {
-                      if (e.code == 'wrong-password') {
-                        print('please enter a valid password');
-                      } else if (e.code == 'user-not found') {
-                        print('User not found');
-                      } else {
-                        print('Somehing else haoppen');
-                        print(e.code);
-                      }
-                    }
-                  },
-                  child: const Text('Login'),
-                ),
-              ]);
-
-            default:
-              return const Text("Loading...");
-          }
-        },
-      ),
+        Padding(
+          padding: const EdgeInsets.all(9.8),
+          child: TextField(
+            controller: _password,
+            obscureText: true,
+            decoration: const InputDecoration(hintText: "Enter Password"),
+            enableSuggestions: false,
+            autocorrect: false,
+          ),
+        ),
+        TextButton(
+          onPressed: () async {
+            final email = _email.text;
+            final password = _password.text;
+            try {
+              final UserCredential = await FirebaseAuth.instance
+                  .signInWithEmailAndPassword(email: email, password: password);
+              print(UserCredential);
+            } on FirebaseAuthException catch (e) {
+              if (e.code == 'wrong-password') {
+                print('please enter a valid password');
+              } else if (e.code == 'user-not found') {
+                print('User not found');
+              } else {
+                print('Somehing else haoppen');
+                print(e.code);
+              }
+            }
+          },
+          child: const Text('Login'),
+        ),
+        TextButton(
+            onPressed: () {
+              Navigator.of(context)
+                  .pushNamedAndRemoveUntil('/register', (route) => false);
+            },
+            child: const Text("Not Registered yet? Register Here!"))
+      ]),
     );
   }
 }
--- lib/views/register_view.dart
+++ lib/views/register_view.dart
@@ -34,73 +34,61 @@ class _RegisterViewState extends State<RegisterView> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text(
-            'Register'), //to get a default blue nav-bar with a title of register
-      ),
-      body: FutureBuilder(
-        future: Firebase.initializeApp(
-          options: DefaultFirebaseOptions.currentPlatform,
+        appBar: AppBar(
+          title: const Text('Register'),
         ),
-        builder: (BuildContext context, AsyncSnapshot<dynamic> snapshot) {
-          switch (snapshot.connectionState) {
-            case ConnectionState.done:
-              return Column(children: [
-                Padding(
-                  padding: const EdgeInsets.all(9.8),
-                  child: TextField(
-                    controller: _email,
-                    keyboardType: TextInputType.emailAddress,
-                    autocorrect: false,
-                    enableSuggestions: false,
-                    decoration: const InputDecoration(
-                      hintText: "Enter Email Address",
-                    ),
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(9.8),
-                  child: TextField(
-                    controller: _password,
-                    obscureText: true,
-                    decoration:
-                        const InputDecoration(hintText: "Enter Password"),
-                    enableSuggestions: false,
-                    autocorrect: false,
-                  ),
-                ),
-                TextButton(
-                  onPressed: () async {
-                    final email = _email.text;
-                    final password = _password.text;
-                    try {
-                      final UserCredential = await FirebaseAuth.instance
-                          .createUserWithEmailAndPassword(
-                              email: email, password: password);
-                      print(UserCredential);
-                    } on FirebaseAuthException catch (e) {
-                      if (e.code == 'weak-password') {
-                        print('weak password');
-                      } else if (e.code == 'email-already-in-use') {
-                        print('email is already in use');
-                      } else if (e.code == 'invalid-email') {
-                        print('invalid email');
-                      } else {
-                        print(e.code);
-                      }
-                    }
-                  },
-                  child: const Text('Register'),
-                ),
-              ]);
-
-            default:
-              return const Center(
-                child: Text("Loading..."),
-              );
-          }
-        },
-      ),
-    );
+        body: Column(children: [
+          Padding(
+            padding: const EdgeInsets.all(9.8),
+            child: TextField(
+              controller: _email,
+              keyboardType: TextInputType.emailAddress,
+              autocorrect: false,
+              enableSuggestions: false,
+              decoration: const InputDecoration(
+                hintText: "Enter Email Address",
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(9.8),
+            child: TextField(
+              controller: _password,
+              obscureText: true,
+              decoration: const InputDecoration(hintText: "Enter Password"),
+              enableSuggestions: false,
+              autocorrect: false,
+            ),
+          ),
+          TextButton(
+            onPressed: () async {
+              final email = _email.text;
+              final password = _password.text;
+              try {
+                final UserCredential = await FirebaseAuth.instance
+                    .createUserWithEmailAndPassword(
+                        email: email, password: password);
+                print(UserCredential);
+              } on FirebaseAuthException catch (e) {
+                if (e.code == 'weak-password') {
+                  print('weak password');
+                } else if (e.code == 'email-already-in-use') {
+                  print('email is already in use');
+                } else if (e.code == 'invalid-email') {
+                  print('invalid email');
+                } else {
+                  print(e.code);
+                }
+              }
+            },
+            child: const Text('Register'),
+          ),
+          TextButton(
+              onPressed: () {
+                Navigator.of(context)
+                    .pushNamedAndRemoveUntil('/login', (route) => false);
+              },
+              child: const Text("Already Registered? Login Here!"))
+        ]));
   }
 }
